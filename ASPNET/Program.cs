7185335b5ce5b4a8f1276d0a29b14fdf76a0ecc9
aston4ever <aston4ever@gmail.com>
2025-11-12 00:25:26 +0300

    Exeptions

--- ASPNET/Program.cs
+++ ASPNET/Program.cs
@@ -2,6 +2,27 @@
 
 class Program {
     static void Main( string[] args ) {
-        Console.WriteLine( "Hello, World!" );
+        try {
+            mySImpleMeth2();
+            mySImpleMeth();
+        } catch ( InvalidOperationException ex ) {
+            Console.WriteLine( "this was intercepted with first catch" );
+            Console.WriteLine( ex.Message );
+        } catch ( NotImplementedException ex ) {
+            Console.WriteLine( "You forgot to implement this method" );
+            Console.WriteLine( ex.Message );
+        }
+        catch ( Exception e ) {
+            Console.WriteLine( e.Message );
+        } 
+        Console.ReadLine();
+    }
+
+    public static void mySImpleMeth() {
+        throw new InvalidOperationException("This method is not working properly");
+    }
+
+    public static void mySImpleMeth2() {
+        throw new NotImplementedException("This method is not implemented");
     }
 }
