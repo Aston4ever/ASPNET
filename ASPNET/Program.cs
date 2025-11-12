225e53e235b4190e368071425889efd937edaf81
Антон Стародубкин <aston4ever@gmail.com>
2025-11-12 10:57:37 +0300

    refactor

--- ASPNET/Program.cs
+++ ASPNET/Program.cs
@@ -1,28 +1,39 @@
 ﻿namespace ASPNET;
 
-class Program {
-    static void Main( string[] args ) {
-        try {
-            mySImpleMeth2();
-            mySImpleMeth();
-        } catch ( InvalidOperationException ex ) {
-            Console.WriteLine( "this was intercepted with first catch" );
-            Console.WriteLine( ex.Message );
-        } catch ( NotImplementedException ex ) {
-            Console.WriteLine( "You forgot to implement this method" );
-            Console.WriteLine( ex.Message );
-        }
-        catch ( Exception e ) {
-            Console.WriteLine( e.Message );
-        } 
-        Console.ReadLine();
-    }
+class Program
+{
+	static void Main(string[] args)
+	{
+		try
+		{
+			mySImpleMeth2();
+			mySImpleMeth();
+		}
+		catch (InvalidOperationException ex)
+		{
+			Console.WriteLine("this was intercepted with first catch");
+			Console.WriteLine(ex.Message);
+		}
+		catch (NotImplementedException ex)
+		{
+			Console.WriteLine("You forgot to implement this method");
+			Console.WriteLine(ex.Message);
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e.Message);
+		}
 
-    public static void mySImpleMeth() {
-        throw new InvalidOperationException("This method is not working properly");
-    }
+		Console.ReadLine();
+	}
 
-    public static void mySImpleMeth2() {
-        throw new NotImplementedException("This method is not implemented");
-    }
+	public static void mySImpleMeth()
+	{
+		throw new InvalidOperationException("This method is not working properly");
+	}
+
+	public static void mySImpleMeth2()
+	{
+		throw new NotImplementedException("This method is not implemented");
+	}
 }
